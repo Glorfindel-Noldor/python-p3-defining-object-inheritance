40d10793ad666890dae2d0488d6fe6da52bee95b
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2024-05-26 12:37:03 -0500

    parent and child

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,11 @@
 from vehicle import Vehicle
 
+
 class Car(Vehicle):
-    pass
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+print(Car.__base__)
+print(int.__base__)
+print(Car.__class__)
+print(int.__class__)
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,12 @@
+
 class Vehicle:
-    pass
+
+    def __init__(self, x, y):
+        self.wheel_size = x
+        self.wheel_number = y
+
+    def go(self):
+        return "vrrrrrrrooom!"
+
+    def fill_up_tank(self):
+        return 'filling up!'
